423eb2e4f6a65c5c83136583208b393ce2daff34
Stefano <tepo@hotmail.it>
2021-03-15 17:43:36 +0100

    Aggiunti comportamenti in base al click su link

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,34 @@
 $(function () {
 
+//variabile che utilizzo per capire se ho clickato o menu un link della navbar
+var clicked = false;
+
 $(".open_menu a").click(
   function() {
     $(".dropdown_menu").removeClass("active");
     $(this).next(".dropdown_menu").addClass("active");
+    //se clicko, i menu non vengono nascosti finchè non clicko di nuovo su un link
+    if ( !clicked ) {
+      clicked = true;
+    } else {
+      clicked = false;
+    }
   }
 );
 
 $(".open_menu").mouseenter(
   function() {
     $(".dropdown_menu").removeClass("active");
-    $(this).children(".dropdown_menu").toggleClass("active");
+    $(this).children(".dropdown_menu").addClass("active");
 
     $(".open_menu").mouseleave(
       function() {
-        $(this).children(".dropdown_menu").toggleClass("active");
+        if ( !clicked ) {
+          $(this).children(".dropdown_menu").removeClass("active");
+        }
       }
     );
+
   }
 );
 
