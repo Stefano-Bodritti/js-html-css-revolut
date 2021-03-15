bcdf9c220ef91a7ce1fbf87883a8126a13b679e8
Stefano <tepo@hotmail.it>
2021-03-15 16:46:15 +0100

    Versione alternativa

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,40 @@
 $(function () {
-
-$(".open_menu").click(
+  
+$(".open_menu a").click(
   function() {
-    $(this).next(".dropdown_menu").slideToggle("fast");
+    $(this).next(".dropdown_menu").toggleClass("active");
   }
 );
 
 $(".open_menu").mouseenter(
   function() {
-    $(this).next(".dropdown_menu").slideToggle("fast");
+    $(this).children(".dropdown_menu").addClass("active");
   }
 );
 
 $(".open_menu").mouseleave(
   function() {
-    $(this).next(".dropdown_menu").slideToggle("fast");
+    $(this).children(".dropdown_menu").removeClass("active");
   }
 );
+
+// $(".open_menu").click(
+//   function() {
+//     $(this).children(".dropdown_menu").slideToggle("fast");
+//   }
+// );
+//
+// $(".open_menu").mouseenter(
+//   function() {
+//     $(this).children(".dropdown_menu").slideToggle("fast");
+//   }
+// );
+//
+// $(".open_menu").mouseleave(
+//   function() {
+//     $(this).children(".dropdown_menu").slideToggle("fast");
+//   }
+// );
 
 
 });
